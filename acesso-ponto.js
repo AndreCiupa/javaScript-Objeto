eef2ced9cc7046f25d44f7ce83dadc899a12aa72
AndreCiupa <andre.ciupa@escola.pr.gov.br>
2026-08-25 15:17:48 +0000

    salvar

--- acesso-ponto.js
+++ acesso-ponto.js
@@ -2,9 +2,16 @@ const cliente = {
     nome: "Andre",
     idade: 16,
     cpf: "112223345",
-    email: "andre@domonio.com",
+    email: "andre@dominio.com",
 };
 
 console.log(`O nome do cliente e ${cliente.nome} e essa pessoa tem ${cliente.idade} anos,`);
 
 console.log(`os 3 primeiros digitos do cpf sao ${cliente.cpf.substring (0,3)}`);
+
+
+const chaves = ["nome", "idade", "cpf", "email"];
+
+chaves.forEach((chave) => {
+    console.log (`A chave ${chave} tem valor ${cliente[chave]}`);
+});

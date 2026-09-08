1d05d5eb2172b969dd07f8d5b6c63015d3ee6843
AndreCiupa <andre.ciupa@escola.pr.gov.br>
2026-09-08 15:16:21 +0000

    salvar

---
+++ aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "joao",
+    idade: 24,
+    email: "joao@Firma.com",
+    telefone: ["1155555555550", "111144444440"],
+}
+
+cliente.endereco = {
+    rua:"Rua Joseph Climbar",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+
+console.log(cliente);
